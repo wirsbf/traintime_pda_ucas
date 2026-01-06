a24edb95dc43e7942a2adafadb69d2f6c40009fa
wirsbf <wirs@stu.xidian.edu.cn>
2026-01-06 20:45:24 +0800

    modified:   android/settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -14,13 +14,26 @@ pluginManagement {
         google()
         mavenCentral()
         gradlePluginPortal()
+        // Fallback mirrors for when Maven Central returns 403
+        maven { url = uri("https://maven.google.com") }
+        maven { url = uri("https://jcenter.bintray.com") }
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositories {
+        google()
+        mavenCentral()
+        // Fallback mirrors
+        maven { url = uri("https://maven.google.com") }
     }
 }
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.11.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
+    id("com.android.application") version "8.7.3" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
 }
 
 include(":app")
